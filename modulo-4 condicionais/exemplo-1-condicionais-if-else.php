a6b9e0a015bb7b965174c333076ee79410ccac55
Alessandro Márcio <timasters10@gmail.com>
2022-03-05 16:42:20 -0300

    aula-20 estruturas condicionais if else .

---
+++ modulo-4 condicionais/exemplo-1-condicionais-if-else.php
@@ -0,0 +1,32 @@
+<?php
+//Estruturas de comandos de fluxo ou estruturas de decisão if else
+
+$qualASuaIdade = 15;
+
+$idadeCriança = 12;
+$idadeMaior = 18;
+$idadeMelhor = 65;
+
+if ($qualASuaIdade < $idadeCriança) {
+
+    echo "Criança";
+
+} else if ($qualASuaIdade < $idadeMaior) {
+
+    echo "Adolescente";
+
+} else if ($qualASuaIdade < $idadeMelhor) {
+
+    echo "Adulto";
+
+} else {
+
+    echo "Idoso";
+
+}
+
+echo "<br>";
+
+echo ($qualASuaIdade < $idadeMaior)?"Menor de Idade":"Maior de Idade";
+
+?>
